9effb84319881e93ccf120f23dc21eaaddba726a
Yash Kumar Gupta <ykgupta2411@gmail.com>
2024-09-16 11:16:49 +0530

    Added a fancy component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './pages/Login';
 import styles from './App.module.scss';
+import UnderConstruction from './components/footer/Under Construction';
 
 function App() {
 	return (
 		<div className={styles.app}>
 			<Login className={styles.login} />
+			<UnderConstruction />
 		</div>
 	);
 }
