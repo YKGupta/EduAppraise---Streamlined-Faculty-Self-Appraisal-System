ef82c2ef0116588afa7c68f5769875997c90e53a
Yash Kumar Gupta <ykgupta2411@gmail.com>
2024-09-18 10:55:40 +0530

    Minor Fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './App.module.scss';
-import UnderConstruction from './components/footer/Under Construction';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
@@ -9,7 +8,6 @@ function App() {
 		<div className={styles.app}>
 			<ToastContainer pauseOnHover={false} theme='dark' progressStyle={{backgroundColor: '#918B6C'}} />
 			<Outlet />
-			<UnderConstruction />
 		</div>
 	);
 }
